238a2888f4ff103abfc22a579883dca34b54d6d9
AbdulQadir92 <a_qadir_jamali@hotmail.com>
2023-03-21 16:24:51 +0500

    Added responsive current stats page of dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,10 @@
-import { CurrentStatsStyled, Main, Page, InfoCard, StatsCard, YellowButton1, BlackButton1, YellowButton2, BlackButton2, CardText, CopyButton } from "@/styles/pages/components/dashboard/CurrentStats.styled"
+import { CurrentStatsStyled, Main, Page, InfoCard, YellowButton1, BlackButton1, YellowButton2, BlackButton2, CardText, CopyButton, StatsCard, StatsButton, CardsContainer, Card } from "@/styles/pages/components/dashboard/CurrentStats.styled"
 import Header from "@/components/dashboard/Header"
 import { useState } from "react"
 import SidebarMobile from '../../components/dashboard/SidebarMobile'
 import SidebarDesktop from "@/components/dashboard/SidebarDesktop"
 import Link from "next/link"
+import content from '../../content/content.json'
 
 
 const CurrentStats = () => {
@@ -11,6 +12,15 @@ const CurrentStats = () => {
     const toggleSidebar = () => {
         setSidebarClass(!sidebarClass);
     }
+
+    const cardImages = [
+        "/assets/images/dashboard/currentStats/statsCard1.svg",
+        "/assets/images/dashboard/currentStats/statsCard2.svg",
+        "/assets/images/dashboard/currentStats/statsCard3.svg",
+        "/assets/images/dashboard/currentStats/statsCard4.svg",
+        "/assets/images/dashboard/currentStats/statsCard5.svg",
+        "/assets/images/dashboard/currentStats/statsCard6.svg"
+    ]
 
     return (
         <CurrentStatsStyled>
@@ -20,26 +30,62 @@ const CurrentStats = () => {
                 <SidebarDesktop activeLink="currentStats" />
                 <Page>
                     <InfoCard>
-                        <YellowButton1 type="button">WALLET INFO</YellowButton1>
+                        <YellowButton1 type="button">{content.dashboard.currentStats.infoCard.yellowButton1}</YellowButton1>
                         <BlackButton1 type="button">
-                            <span>your trx wallet address</span>
-                            <span>TEvG6VpgitzigWyqvhmGcihLf6WBEM1Q6s</span>
+                            <span>{content.dashboard.currentStats.infoCard.blackButton1.span1}</span>
+                            <span>{content.dashboard.currentStats.infoCard.blackButton1.span2}</span>
                         </BlackButton1>
                         <YellowButton2 type="button">
-                            <span>Upline Partner Wallet</span>
-                            <span>TEvG6VpgitzigWyqvhmGcihLf6WBEM1Q6s</span>
+                            <span>{content.dashboard.currentStats.infoCard.yellowButton2.span1}</span>
+                            <span>{content.dashboard.currentStats.infoCard.yellowButton2.span2}</span>
                         </YellowButton2>
                         <BlackButton2 type="button">
-                            <span>Your Referral Link</span>
-                            <Link href="https://trontrx.world/?ref=TEvG6VpgitzigWyqvhmGcihLf6WBEM1Q6s">https://trontrx.world/?ref=TEvG6VpgitzigWyqvhmGcihLf6WBEM1Q6s</Link>
+                            <span>{content.dashboard.currentStats.infoCard.blackButton2.span1}</span>
+                            <Link href="https://trontrx.world/?ref=TEvG6VpgitzigWyqvhmGcihLf6WBEM1Q6s">{content.dashboard.currentStats.infoCard.blackButton2.link}</Link>
                         </BlackButton2>
                         <CardText>
-                            Share this link with your partners to get referral rewards. You should have active deposit to unlock affiliate bonuses!
+                            {content.dashboard.currentStats.infoCard.card1Text}
                         </CardText>
-                        <CopyButton>Copy</CopyButton>
+                        <CopyButton>{content.dashboard.currentStats.infoCard.copyButton}</CopyButton>
                     </InfoCard>
                     <StatsCard>
-
+                        <StatsButton type="button">{content.dashboard.currentStats.statsCard.yellowButton}</StatsButton>
+                        <CardsContainer>
+                            <div>
+                                <Card>
+                                    <img src={cardImages[0]} alt="..." />
+                                    <p>{content.dashboard.currentStats.statsCard.cards[0].title}</p>
+                                    <div>{content.dashboard.currentStats.statsCard.cards[0].number}</div>
+                                </Card>
+                                <Card>
+                                    <img src={cardImages[1]} alt="..." />
+                                    <p>{content.dashboard.currentStats.statsCard.cards[1].title}</p>
+                                    <div>{content.dashboard.currentStats.statsCard.cards[1].number}</div>
+                                </Card>
+                                <Card>
+                                    <img src={cardImages[2]} alt="..." />
+                                    <p>{content.dashboard.currentStats.statsCard.cards[2].title}</p>
+                                    <div>{content.dashboard.currentStats.statsCard.cards[2].number}</div>
+                                </Card>
+                            </div>
+                            <div>
+                                <Card>
+                                    <img src={cardImages[3]} alt="..." />
+                                    <p>{content.dashboard.currentStats.statsCard.cards[3].title}</p>
+                                    <div>{content.dashboard.currentStats.statsCard.cards[3].number}</div>
+                                </Card>
+                                <Card>
+                                    <img src={cardImages[4]} alt="..." />
+                                    <p>{content.dashboard.currentStats.statsCard.cards[4].title}</p>
+                                    <div>{content.dashboard.currentStats.statsCard.cards[4].number}</div>
+                                </Card>
+                                <Card>
+                                    <img src={cardImages[5]} alt="..." />
+                                    <p>{content.dashboard.currentStats.statsCard.cards[5].title}</p>
+                                    <div>{content.dashboard.currentStats.statsCard.cards[5].number}</div>
+                                </Card>
+                            </div>
+                        </CardsContainer>
                     </StatsCard>
                 </Page>
             </Main>
--- src/styles/pages/components/dashboard/CurrentStats.styled.tsx
+++ src/styles/pages/components/dashboard/CurrentStats.styled.tsx
@@ -14,19 +14,30 @@ export const Main = styled.div`
 export const Page = styled.main`
     width: 100%;
     padding-left: 30px;
+
+    @media (max-width: 768px) {
+        padding-left: 15px;
+    }
 `
 
 export const InfoCard = styled.div`
     width: 96%;
-    height: 519px;
+    height: 529px;
     background: #FFFFFF;
-    border-radius: 20.9624px;
+    border-radius: 24.5px;
     text-align: center;
-
-    margin-bottom: 30px;
+    margin-bottom: 21px;
+    margin-top: 10px;
+    border: 5px solid #FBBD18;
 
     @media (max-width: 1200px) {
         height: 620px;
+    }
+
+    @media (max-width: 768px) {
+        margin-top: 29px;
+        margin-bottom: 32px;
+        border: 3px solid #FBBD18;
     }
 
     @media (max-width: 576px) {
@@ -48,7 +59,7 @@ export const YellowButton1 = styled.button`
     text-transform: uppercase;
     color: #000000;
 
-    margin-top: 15px;
+    margin-top: 10px;
     margin-right: 40px;
     margin-left: 40px;
 
@@ -147,12 +158,14 @@ export const CopyButton = styled.button`
     line-height: 33px;
     text-transform: uppercase;
     color: #F2A71B;
-    margin-top: 36px;
+    cursor: pointer;
+
+    margin-top: 32px;
     margin-left: auto;
     margin-right: auto;
 
     @media (max-width: 1200px) {
-        margin-top: 40px;
+        margin-top: 35px;
     }
 
     @media (max-width: 576px) {
@@ -161,5 +174,140 @@ export const CopyButton = styled.button`
 `
 
 export const StatsCard = styled.div`
-    
+    width: 96%;
+    height: 519px;
+    background: #FFFFFF;
+    border-radius: 20.9624px;
+    border: 5px solid #FBBD18;
+    margin-bottom: 200px;
+
+    @media (max-width: 992px) {
+        margin-bottom: 100px;
+    }
+
+    @media (max-width: 768px) {
+        height: auto;
+        padding-bottom: 31px;
+        margin-bottom: 75px;
+        border: 3px solid #FBBD18;
+    }
+`
+
+export const StatsButton = styled(YellowButton1)`
+    margin-top: 40px;
+    margin-bottom: 42px;
+
+    @media (max-width: 768px) {
+        margin-top: 13px;
+        margin-bottom: 28px;
+    }
+`
+
+export const CardsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    row-gap: 35px;
+
+    > div {
+        display: flex;
+        column-gap: 70px;
+        margin-left: 40px;
+        margin-right: 40px
+    }
+
+    @media (max-width: 1300px) {
+        > div {
+            column-gap: 40px;
+        }
+    }
+
+    @media (max-width: 1200px) {
+        row-gap: 20px;
+
+        > div {
+            column-gap: 20px;
+            justify-content: space-between;
+            margin-left: 14px;
+            margin-right: 14px;
+        }
+    }
+
+    @media (max-width: 768px) {
+        > div {
+            flex-direction: column;
+            row-gap: 20px;
+            margin-left: 40px;
+            margin-right: 40px
+        }
+    }
+`
+
+export const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    width: 30%;
+    height: 147px;
+    background: #000000;
+    box-shadow: -10.7234px 12.1532px 22.8766px rgba(0, 0, 0, 0.25), inset 0.714894px 2.14468px 11.4383px rgba(0, 0, 0, 0.25);
+    backdrop-filter: blur(3.57447px);
+    /* Note: backdrop-filter has minimal browser support */
+    border-radius: 4.28936px;
+
+    img {
+        width: 37.5px;
+        height: 37.5px;
+    }
+
+    p {
+        font-family: 'Poppins', sans-serif;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 18.54px;
+        line-height: 28px;
+        text-transform: uppercase;
+        color: #FFFFFF;
+    }
+
+    div {
+        font-family: 'Poppins', sans-serif;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 20.54px;
+        line-height: 31px;
+        text-transform: uppercase;
+        color: #FFFFFF;
+    }
+
+    @media (max-width: 1300px) {
+        p {
+            font-size: 17px;
+        }
+    }
+
+    @media (max-width: 1200px) {
+        width: 32%;
+
+        p {
+            font-size: 15.5px;
+        }
+    }
+
+    @media (max-width: 768px) {
+        width: 100%;
+
+        p {
+            font-size: 18.54px;
+        }
+    }
+
+    @media (max-width: 576px) {
+        width: 100%;
+
+        p {
+            font-size: 16.54px;
+        }
+    }
 `
